d67a39fc7f7b7c4c6bdc44ec51a2a722cef65954
StefanoMela <stefano.mela25@gmail.com>
2023-08-21 17:28:11 +0200

    small adjs

--- JS/script.js
+++ JS/script.js
@@ -6,25 +6,34 @@
 
 
 const newDiv = document.getElementById("box-target");
-const gridButton = document.getElementById("grid-button-easy");
+const gridButton = document.getElementById("grid-button");
 
 gridButton.addEventListener("click", function () {
 
     for (let i = 1; i <= 100; i++) {
-    
-        let output = i;
-        let myClass = "background";
-        const myCell = document.createElement("div");
-        myCell.innerHTML = output;
-        myCell.classList.add("classic__box");
+        const myCell = createCell(i);
         newDiv.append(myCell);
-        
-        myCell.addEventListener("click", function() {
-            
-            myCell.classList.add(myClass);
-            console.log("Hai cliccato il numero " + output);
-        })
     }
 });
 
+function createCell(index) {
+
+    const myCell = document.createElement("div");
+    myCell.innerHTML = index;
+    myCell.classList.add("classic__box");
+    let myClass = "background";
+    myCell.addEventListener("click", function () {
+
+        myCell.classList.add(myClass);
+        console.log("Hai cliccato il numero " + i);
+    })
+
+    return myCell;
+};
+
 // BONUS
+
+
+const easyMode = document.getElementById("easy")
+const mediuMode = document.getElementById("medium")
+const hardMode = document.getElementById("hard")
